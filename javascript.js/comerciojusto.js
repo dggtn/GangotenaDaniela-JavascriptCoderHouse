806220960586924c4e9af9b63f8657ca644613d4
Daniela <dggtn9@gmail.com>
2023-12-05 19:13:17 -0300

    avances

---
+++ javascript.js/comerciojusto.js
@@ -0,0 +1,13 @@
+// //Ejercicio for : mostrar en la consola la tabla de multiplicación del numero
+
+// let entrada =prompt("Ingrese un numero");
+// for entrada
+
+
+
+
+
+// //Ejercicio while: mostrar en la consola si el numero es par o impar y termina cuando escribe fin 
+// let entrada =prompt("Ingrese un numero");
+// while entrada != fin 
+// console.log(entrada)
