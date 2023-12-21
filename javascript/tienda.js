5e8f2c904c2851860955703d00cd09c50249f0ce
Daniela <dggtn9@gmail.com>
2023-12-21 19:50:49 -0300

    cambiar var por let en javascript

--- javascript/tienda.js
+++ javascript/tienda.js
@@ -54,7 +54,7 @@ function TiendaEnLinea(categorias, productos) {
     this.productos = productos;
 
     this.buscarPrecioDeProducto = function(nombre) {
-        var producto = this.productos.find((producto) => producto.nombre == nombre);
+        let producto = this.productos.find((producto) => producto.nombre == nombre);
         return producto.precio;
     }
     this.obtenerProductosPorCategoria = function (nombre){
@@ -77,43 +77,43 @@ function Categoria(nombre) {
 
 //instanciación de objetos:
 
-var frutasSecas = new Categoria("Frutas Secas");
-var lacteos = new Categoria("Lacteos");
-var especias = new Categoria("Especias");
-var frutasDesh = new Categoria("Frutas Deshidratadas");
-var mantequilla = new Categoria("Mantequillas de Frutos Secos");
+let frutasSecas = new Categoria("Frutas Secas");
+let lacteos = new Categoria("Lacteos");
+let especias = new Categoria("Especias");
+let frutasDesh = new Categoria("Frutas Deshidratadas");
+let mantequilla = new Categoria("Mantequillas de Frutos Secos");
 
 
-var mantequillaDeAlmendras = new Producto("Mantequilla de Almendras", 12,mantequilla );
-var mantequillaDeAvellanas = new Producto("Mantequilla de Avellanas", 12.50,mantequilla );
-var mantequillaDeCashew = new Producto("Mantequilla de Cashew", 13,mantequilla );
+let mantequillaDeAlmendras = new Producto("Mantequilla de Almendras", 12,mantequilla );
+let mantequillaDeAvellanas = new Producto("Mantequilla de Avellanas", 12.50,mantequilla );
+let mantequillaDeCashew = new Producto("Mantequilla de Cashew", 13,mantequilla );
 
-var almendras = new Producto("Almendras", 21, frutasSecas);
-var nueces = new Producto("Nueces", 18, frutasSecas);
-var avellanas = new Producto("Avellanas", 54, frutasSecas);
+let almendras = new Producto("Almendras", 21, frutasSecas);
+let nueces = new Producto("Nueces", 18, frutasSecas);
+let avellanas = new Producto("Avellanas", 54, frutasSecas);
 
-var frutillas = new Producto("Frutillas", 8.50, frutasDesh);
-var ananas = new Producto("Ananas", 4.5, frutasDesh);
-var moras = new Producto("Moras", 4.5, frutasDesh);
+let frutillas = new Producto("Frutillas", 8.50, frutasDesh);
+let ananas = new Producto("Ananas", 4.5, frutasDesh);
+let moras = new Producto("Moras", 4.5, frutasDesh);
 
-var lecheAlmendras = new Producto("Leche de Almendras", 5.5, lacteos);
-var lecheCoco = new Producto("Leche de Coco", 5.5, lacteos);
-var lecheAvena= new Producto("Leche de Avena", 5.5, lacteos);
+let lecheAlmendras = new Producto("Leche de Almendras", 5.5, lacteos);
+let lecheCoco = new Producto("Leche de Coco", 5.5, lacteos);
+let lecheAvena= new Producto("Leche de Avena", 5.5, lacteos);
 
-var canela = new Producto("Canela", 2.5, especias);
-var pimenton = new Producto("Pimentón", 2.5, especias);
-var salHimalaya = new Producto("Sal Himalaya", 4.5, especias);
+let canela = new Producto("Canela", 2.5, especias);
+let pimenton = new Producto("Pimentón", 2.5, especias);
+let salHimalaya = new Producto("Sal Himalaya", 4.5, especias);
 
-var categorias = [frutasSecas, frutasDesh, especias, lacteos, mantequilla];
-var productos = [mantequillaDeAlmendras, mantequillaDeAvellanas, mantequillaDeCashew, almendras, nueces, avellanas, frutillas,
+let categorias = [frutasSecas, frutasDesh, especias, lacteos, mantequilla];
+let productos = [mantequillaDeAlmendras, mantequillaDeAvellanas, mantequillaDeCashew, almendras, nueces, avellanas, frutillas,
      ananas, moras, lecheAlmendras, lecheCoco, lecheAvena, canela, pimenton, salHimalaya];
 
 
-var tiendaEnLinea = new TiendaEnLinea(categorias, productos);
+let tiendaEnLinea = new TiendaEnLinea(categorias, productos);
 
 //Ejecución del programa:
 
-var mensajeDeBienvenida =`Bienvenidos a la tienda en linea
+let mensajeDeBienvenida =`Bienvenidos a la tienda en linea
 Escribe el numero de la opción que buscas:
 1 -Frutas Secas
 2 -Lacteos
@@ -126,10 +126,10 @@ Para salir de la tienda escribe salir
 para finalizar tu compra escribe finalizar
 `
 
-var opcionElegida = prompt(mensajeDeBienvenida);
-var productoElegido;
-var cantidadElegida = 0;
-var montoAPagar = 0;
+let opcionElegida = prompt(mensajeDeBienvenida);
+let productoElegido;
+let cantidadElegida = 0;
+let montoAPagar = 0;
 
 while(opcionElegida != "salir") {
 
@@ -159,8 +159,8 @@ while(opcionElegida != "salir") {
 }
 
 function procesarOpcion(categoria) {
-    var listaDeProductos = ""
-        var productos = tiendaEnLinea.obtenerProductosPorCategoria(categoria);
+    let listaDeProductos = ""
+        let productos = tiendaEnLinea.obtenerProductosPorCategoria(categoria);
         productos.forEach(producto => {
             listaDeProductos += `${producto.nombre} ${producto.precio} USD\n`
         });
