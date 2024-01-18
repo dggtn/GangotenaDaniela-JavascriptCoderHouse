5cc3ce07a1d9b2ac614dd2b9cad8d0a78f8b0de2
Daniela <dggtn9@gmail.com>
2024-01-18 12:14:34 -0300

    nuevo usuario

--- javascript/usuario.js
+++ javascript/usuario.js
@@ -1,9 +1,9 @@
 class Usuario {
-    constructor(id, nombre, descripcion, imagen, precio) {
+    constructor(id, nombre, email, password) {
         this.id = id;
         this.nombre = nombre;
-        this.email = mail;
-        this.password = contrasena;
+        this.email = email;
+        this.password = password;
     }
 }
 
@@ -12,12 +12,57 @@ class UsuariosRegistrados {
     constructor(){
         this.usuarios = [];
     }
+
     agregarUsuario(usuario){
         this.usuarios.push(usuario);
     }
 
-    guardarusuario() {
-        return this.productos;
+   
+    obtenerUsuarioPorId(id) {
+        return this.usuarios.find((usuario) => usuario.id == id)
     }
 
 }
+
+
+let juan  = new Usuario(1,"Juan","juan@yahoo.com","123456");
+let maria  = new Usuario(1,"maria","maria@yahoo.com","123456");
+let alejandro  = new Usuario(1,"alejandro","alejandron@yahoo.com","123456");
+
+
+let  UsuariosRegistrados= new  UsuariosRegistrados();
+UsuariosRegistrados.agregarUsuario(juan);
+UsuariosRegistrados.agregarUsuario(maria);
+UsuariosRegistrados.agregarUsuario(alejandro);
+
+let alertaIngreso;
+
+function logearse(id){
+
+    let UsuariosRegistrados = [];
+   
+    let usuariosString =  localStorage.getItem("Usuarios");
+
+    if (usuariosString){
+        UsuariosRegistrados = JSON.parse(usuariosString);
+    }
+    const usuario= UsuariosRegistrados.find((unUsuario) => unUsuario.id === Number(id));
+
+    if (usuario){
+        let alertaIngreso = document.getElementById("alertaIngreso" + usuario.id);
+        
+        Swal.fire("Ya estas registrado");
+        
+        let UsuarioPorId = favoritos.map((unUsuario) => unUsuario.id)
+        const index = UsuarioPorId.indexOf(Number(id));
+        UsuariosRegistrados.splice(index,1)
+    } else{
+       Swal.fire("Debes registrarte"); 
+    
+    }
+    
+    localStorage.setItem("usuarios",JSON.stringify(usuario));
+}
+
+   
+
