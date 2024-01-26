4129c7cecfc8bbf055ad45fd01c562383477efe4
Daniela <dggtn9@gmail.com>
2024-01-26 13:28:32 -0300

    newsletter

--- javascript/newsletter.js
+++ javascript/newsletter.js
@@ -3,7 +3,8 @@ let correoelectronico2;
 
 //validar formularios
 
-function newsletter(){
+function newsletter(e){
+    e.preventDefault();
     let formularioValido = true;
     let emailValido = true
     let nombreYApellido= document.getElementById("nombreYApellido").value;
@@ -23,6 +24,15 @@ function newsletter(){
     }
     
     if (formularioValido == true){
+        let formulario =  document.getElementById('contact-form')
+        emailjs.init('xfBN8QaHVdIGL1WLz');
+        formulario.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('email_service', 'contact_form', formulario)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
         Swal.fire("Gracias " + nombreYApellido + " por suscribirte a nuestro newsletter") 
         document.getElementById("nombreYApellido").value=""
         document.getElementById("correoelectronico").value=""
@@ -34,6 +44,7 @@ function newsletter(){
             Swal.fire ("Formato de email incorrecto")
         }
     }
+
 }
 
 function validarEmail(correoelectronico){
@@ -42,3 +53,10 @@ function validarEmail(correoelectronico){
 	let expresionRegular =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     return expresionRegular.test(correoelectronico);
 }
+
+(function registrarseAlNewsletter() {
+    // https://dashboard.emailjs.com/admin/account
+    
+})();
+
+  
